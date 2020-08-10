5b9d29a262e47ce5969fca0f4e242005979e0c6b
Kayo Kalinauskas <kalinauskas@live.com>
2020-08-10 02:53:29 -0300

    Formating

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,34 +4,30 @@ import "./styles.css";
 import api from "../../services/api";
 
 export interface Teacher {
-    id: number;
-    avatar: string;
-    bio: string;
-    cost: number;
-    name: string;
-    subject: string;
-    whatsapp: string;
-  }
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
 
 interface TeacherItemProps {
   teacher: Teacher;
-  }
+}
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
-    function createNewConnection() {
-        api.post('connections', {
-            user_id: teacher.id,
-        })
-    }
-
+  function createNewConnection() {
+    api.post("connections", {
+      user_id: teacher.id,
+    });
+  }
 
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src={teacher.avatar}
-          alt={teacher.name}
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
           <strong>{teacher.name}</strong>
           <span>{teacher.subject}</span>
@@ -42,11 +38,10 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
         <p>
           Preço/hora <strong>R$ {teacher.cost}</strong>
         </p>
-        <a 
-        target="_blank"
-        onClick={createNewConnection} 
-        href={`https://wa.me/${teacher.whatsapp}`}
-
+        <a
+          target="_blank"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
         >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -8,15 +8,12 @@ import Input from "../../components/Input";
 import Select from "../../components/Select";
 import api from "../../services/api";
 
-
-
-
 function TeacherList() {
   const [teachers, setTeachers] = useState([]);
 
-  const [subject, setSubject] = useState('');
-  const [week_day, setWeekday] = useState('');
-  const [time, setTime] = useState('');
+  const [subject, setSubject] = useState("");
+  const [week_day, setWeekday] = useState("");
+  const [time, setTime] = useState("");
 
   async function searchTeachers(e: FormEvent) {
     e.preventDefault();
@@ -88,9 +85,9 @@ function TeacherList() {
       </PageHeader>
 
       <main>
-          {teachers.map((teacher: Teacher) => {
-              return <TeacherItem key={teacher.id} teacher={teacher} />
-          })}
+        {teachers.map((teacher: Teacher) => {
+          return <TeacherItem key={teacher.id} teacher={teacher} />;
+        })}
       </main>
     </div>
   );
